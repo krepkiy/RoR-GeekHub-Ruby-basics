c7166dac31a6c9cdd61576eb4e1f1998417e23ff
«krepkiy» <fansity@rambler.ru>
2013-10-12 18:12:44 +0300

    Week1 Задание1

--- 1.rb
+++ 1.rb
@@ -3,14 +3,11 @@
 day = 1000000000 / (3600 * 24)
 puts 'Человек прожил на белом свете всего:'
 puts 'дней - ' + day.to_s
-#puts day
 
 years = day / 365
 puts 'лет - ' + years.to_s
-#puts years
 
 visokosn = years / 4
-#puts visokosn
 
 ostday = day - ((years - visokosn) * 365 + visokosn * 366)
 ostmonth = ostday / 30
@@ -18,7 +15,6 @@ month = years * 12 + ostmonth
 puts 'месяцев - ' + month.to_s
 newday = ostday - 31 - 28 - 31 - 30 - 31 - 30 - 31 - 31
 puts 'Итого,человек прожил - ' + years.to_s + ' год,' + ostmonth.to_s + ' месяцев и ' + newday.to_s + ' дней'
-#puts ostday
 
 puts 'Проверяем колличество всего прожитых дней:'
 proverka = visokosn * 366 + (years - visokosn) * 365 + (ostday - newday) + newday
